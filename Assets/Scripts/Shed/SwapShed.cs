8a0d8f23ccaa5cdf59418f549726341a43d85171
KVDK345 <kyan.vanderkelen3@gmail.com>
2026-04-01 14:55:30 +0200

    New Shed Prefab

---
+++ Assets/Scripts/Shed/SwapShed.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapShed : MonoBehaviour
+{
+    public GameObject defaultShed;
+    public GameObject openShed;
+
+    private HashSet<GameObject> playersInside = new HashSet<GameObject>();
+
+    void Start()
+    {
+        UpdateState();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playersInside.Add(other.gameObject);
+            UpdateState();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playersInside.Remove(other.gameObject);
+            UpdateState();
+        }
+    }
+
+    void UpdateState()
+    {
+        bool playerInside = playersInside.Count > 0;
+
+        openShed.SetActive(playerInside);
+        defaultShed.SetActive(!playerInside);
+    }
+}
