2b1d8205c4130dfcd93c53e9fcf71f545821347f
Bordocklius <miguel.dhaenens@hotmail.com>
2026-04-01 16:35:53 +0200

    Working on tilemanager

---
+++ Assets/Scripts/Farming/TileManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public static TileManager Instance;
+
+
+    [Space(10), Header("Bomb settings")]
+    public int MaxBombs = 4;
+    public int BombAmount;
+    public GameObject BombObject;
+
+    public List<FarmTile> FarmTiles = new List<FarmTile>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void AddTilesToList(FarmTile tile)
+    {
+        FarmTiles.Add(tile);
+    }
+
+    public void GenerateBombs()
+    {
+        if(FarmTiles.Count < 4)
+            return;
+
+        List<FarmTile> randomTiles = FarmTiles.OrderBy(x => UnityEngine.Random.value).Take(4).ToList();
+        
+        foreach(FarmTile tile in randomTiles)
+        {
+            tile.BombObject = BombObject;
+            tile.HasBomb = true;
+            tile.gameObject.name = "Tile with mine";
+        }
+    }
+}
